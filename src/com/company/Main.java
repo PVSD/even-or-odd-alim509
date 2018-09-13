b0fe6bf3baa86cae0c22d85ad1931f0f40038f6f
al5091 <angelovinolim@gmail.com>
2018-09-13 11:23:19 -0400

    fgas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
+import java.util.*;
 
-public class Main {
+class Tester {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner kbInput = new Scanner(System.in);
+        System.out.println("Enter an integer.");
+        String integer = kbInput.next();
+        do {
+            try {
+                if (Integer.parseInt(integer) )
+                }
+            }catch(Exception e){
+                System.out.println("That is not an integer! Pick any whole number to proceed.");
+                firstNum = kbInput.next();
+            }
+        } while (firstNumInt < 1 || firstNumInt > 10);
+
     }
 }
